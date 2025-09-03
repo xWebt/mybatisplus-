052b2893844b1abfcdfb0e32f3c424756018aa6c
Wangwenbo <gg2050361034@gmail.com>
2025-09-04 05:23:55 +0800

    入门案例原始代码

--- src/main/java/com/itheima/mp/mapper/UserMapper.java
+++ src/main/java/com/itheima/mp/mapper/UserMapper.java
@@ -1,12 +1,11 @@
 package com.itheima.mp.mapper;
 
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.itheima.mp.domain.po.User;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
-public interface UserMapper {
+public interface UserMapper{
 
     void saveUser(User user);
 
